adfa5f3712046e0ade6e5db480cc1667742ffbd6
Yeats <wstgzy@126.com>
2019-08-04 09:02:53 +0800

    将远程的index.css改为本地的index.css

---
+++ qingcheng_parent/qingcheng_common_service/src/main/java/com/qingcheng/util/Demo.java
@@ -0,0 +1,17 @@
+package com.qingcheng.util;
+
+/**
+ * Author: Feiyue
+ * Date: 2019/8/4 0:24
+ * Desc:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        IdWorker idWorker = new IdWorker();
+        long id = 0L;
+        for (int i = 0; i < 10000; i++) {
+            id = idWorker.nextId();
+            System.out.println(id);
+        }
+    }
+}
